e8e0d475d58926e1cb3d84edfb95cb0df56d21fe
unknown <sofikiereme43@gmail.com>
2026-08-25 20:30:00 -0400

    first commit

---
+++ app/src/main/java/bo/edu/uajms/marianagutierrez/a2026_tresenraya/MainActivity.kt
@@ -0,0 +1,11 @@
+package bo.edu.uajms.marianagutierrez.a2026_tresenraya
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
